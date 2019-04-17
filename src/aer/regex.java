8759cb1be58742dcc8393675db668ddd473aebaa
chm <3036643270@qq.com>
2019-04-17 19:59:55 +0800

    第4题（1）

--- src/aer/regex.java
+++ src/aer/regex.java
@@ -7,7 +7,7 @@ public class regex {
 	}
     public static void tel()
     {
-    	String telnumber="(0000)0000-0000";
+    	String telnumber="13400945171";
     	String regex="^^1[34578]\\d{9}|\\d{3}-\\d{4}-\\d{4}$";
     	System.out.println(telnumber.matches(regex));
     }
