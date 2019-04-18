a6e8289fddfd6312608c7acfcda56d1df37b25d0
chm <3036643270@qq.com>
2019-04-18 20:57:16 +0800

    4(8)

--- src/aer/regex.java
+++ src/aer/regex.java
@@ -1,12 +1,15 @@
 package aer;
 
+import java.util.regex.Pattern;
+
 public class regex {
 	
 	 public static void main (String[] args) {
 
     	test1();
     	test3();
-    	test6();}
+    	test6();
+    	test8();}
     	
     	public static void test1()
     	{String s="(0000)0000-0000";
@@ -30,6 +33,29 @@ public class regex {
     				 String s5="abcab435ca56"; 
         			 
      				System.out.println(s5.matches("(.*)(abc)")); 
+
+
+    
+	}
+    	public static <Matcher> void test8()
+    	{
+    		 String  s8="ae256dd¡ªw348e6";
+    		 int[] arr=new int[20];
+    		 Pattern p1=Pattern.compile("(\\d{1,4})");
+    		 @SuppressWarnings("unchecked")
+			Matcher m1= (Matcher) p1.matcher(s8);
+    		 int i=0;
+    		 while(((java.util.regex.Matcher) m1).find()) { 
+    			 			 int j=0; 
+    			  			 j=Integer.parseInt(((java.util.regex.Matcher) m1).group()); 
+    			  			 arr[i]=j; 
+    			  			 i++; 
+    			  		 } 
+    			  		 for(int k=0;k<i;k++) { 
+    						 System.out.println("Êý×é["+k+"]:"+arr[k]); 
+    			 		 } 
+
+    		 
 
 
     
