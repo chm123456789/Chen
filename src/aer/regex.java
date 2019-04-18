27abd8f5d087d4c326cd0f2318652abbf0042585
chm <3036643270@qq.com>
2019-04-18 20:35:49 +0800

    4(3)

--- src/aer/regex.java
+++ src/aer/regex.java
@@ -1,14 +1,27 @@
 package aer;
 
 public class regex {
-	public static void main (String[] args)
-	{
-		tel();
-	}
-    public static void tel()
-    {
-    	String telnumber="13400945171";
-    	String regex="^^1[34578]\\d{9}|\\d{3}-\\d{4}-\\d{4}$";
-    	System.out.println(telnumber.matches(regex));
+	
+	 public static void main (String[] args) {
+
+    	test1();
+    	test3();}
+    	
+    	public static void test1()
+    	{String s="(0000)0000-0000";
+		System.out.println(s.matches("\\(\\d{4}\\)\\d{4}\\-\\d{4}"));
+	
     }
+    	public static void test3()
+    	{
+    		 String s2="abcabcabc"; 
+    			 
+    		 		String s22=s2.replaceAll("(abc)(\\1)*","$1"); 
+    				System.out.println(s22); 
+
+    
+	}
+    
+		
+	
 }
