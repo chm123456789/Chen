a7bf94b3832828c8f4675dc9f5aad68055a8e73c
chm <3036643270@qq.com>
2019-04-21 16:06:36 +0800

    第二次

--- src/text/TestException.java
+++ src/text/TestException.java
@@ -1,23 +1,39 @@
 package text;
 
+class MyException extends Exception{
+
+	
+	
+	
+
+	
+		
+	}
+	
 
 class MyTool{
-	public static int parseInt(String str)
+	public static int parseInt(String str) throws MyException
 	{
 		int result=0;
 		int i;
 		for(i=0;i<str.length();i++) {
 			
 			char ch=str.charAt(i);
-			result=result*10+ch-'0';
+			if(ch>='0'&&ch<='9')result=result*10+ch-'0';
+			else throw new MyException();
 		}
 		return result;
 	}
 }
 public class TestException {
 	public static void main(String[] args) {
-		System.out.println(MyTool.parseInt("345a"));
-		
+		try {
+			System.out.println(MyTool.parseInt("345a"));
+			
+		}catch(MyException e) {
+			// TODO Auto-generated catch block
+			 e.printStackTrace();
+		}
 	}
 
 }
