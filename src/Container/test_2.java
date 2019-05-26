0187b5e2591a4c11e19c52cacc9d8f03871b693a
chm <3036643270@qq.com>
2019-05-26 23:32:41 +0800

    第二题第三次提交

--- src/Container/test_2.java
+++ src/Container/test_2.java
@@ -1,10 +1,31 @@
 package Container;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class test_2 {
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Map<Stu1, String> map = new HashMap<Stu1, String>();
 		
-	}
+		Stu1 s1 = new Stu1("刘里",20);
+		Stu1 s2 = new Stu1("小夏",19);
+		Stu1 s3 = new Stu1("娜娜",18);
+		map.put(s1,"归属地：澳大利亚");
+		map.put(s2,"归属地：伦敦");
+		map.put(s3,"归属地：英国");
+		System.out.println("KeySet遍历:");
+		for(@SuppressWarnings("unused") Stu1 key:map.keySet())
+			{
+			System.out.println(key+"  "+map.get(key));
+			}
+		System.out.println();
+	
+	}		
+	
 }
    class Stu1
    {
@@ -32,7 +53,13 @@ public class test_2 {
 		{
 			return age;
 		}
-	
-		public void setAge(int age) {
-			}
-}
+		public void setAge(int age)
+		{
+			this.age=age;
+		}
+		public String toString() {
+			return "Student_1 [name=" + name + ", age=" + age + "]";
+		}}
+
+   
+
