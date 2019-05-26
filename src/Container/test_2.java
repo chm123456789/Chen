44cd3cf83e933271ae69428d0b8165b8f2912e5a
chm <3036643270@qq.com>
2019-05-26 20:55:11 +0800

    容器 第二题，第二次提交

---
+++ src/Container/test_2.java
@@ -0,0 +1,38 @@
+package Container;
+
+public class test_2 {
+	public static void main(String[] args) 
+	{
+		
+	}
+}
+   class Stu1
+   {
+	String name;
+	int age;
+	
+	public Stu1(String name, int age)
+	{
+		super();
+		this.name = name;
+		this.age = age;
+			
+	}
+	
+		public String getName()
+		{
+			return name;
+		}
+	
+		public void setName(String name)
+		{
+		this.name = name;
+			}
+		public int getAge()
+		{
+			return age;
+		}
+	
+		public void setAge(int age) {
+			}
+}
