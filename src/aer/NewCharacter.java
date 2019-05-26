691e56cc32037f7a4ddc5e02445da52b98866c6d
chm <3036643270@qq.com>
2019-05-26 13:46:21 +0800

    新特性第二次

--- src/aer/NewCharacter.java
+++ src/aer/NewCharacter.java
@@ -1,5 +1,34 @@
 package aer;
 
-public class NewCharacter {
+import java.util.function.Consumer;
 
+
+class Apple
+ {
+	 String color;
+	 int weight;
+	 public Apple(String color,int weight)
+	 {
+		 super();
+		 this.color=color;
+		 this.weight=weight;
+	 }
+	 public String toString() {
+		return "Apple [color=" + color + ", weight=" + weight + "]";
+		 
+	 }
+ }
+public class NewCharacter {
+      public static void ConsumerApple(Apple[] apps,Consumer<Apple> c)
+      {
+    	  for(@SuppressWarnings("unused") Apple app:apps)
+    	  {
+    		  c.accept(app);
+    	  }
+      }
+      public static void main(String[] args)
+      {
+    	  
+      }
+      
 }
