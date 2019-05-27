bdf93c88f159c466e21c550efa97dc7a7800fc03
chm <3036643270@qq.com>
2019-05-27 14:24:49 +0800

    第二次提交

--- src/Container/test3.java
+++ src/Container/test3.java
@@ -1,8 +1,16 @@
 package Container;
 
+import java.util.HashMap;
+
 public class test3 {
 	public static void main(String[] args)
 	{
+		HashMap map=new HashMap();
+		people p1=new people("¹þ¹þ",0);
+		people p2=new people("¹¨¸ö",0);
+		
+		people p3=new people("·ç¸ñ",0);
+		
 		
 	}
 
