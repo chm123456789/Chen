8b0210de7e8ab4326d77cf85933f01ebeb53a5e9
chm <3036643270@qq.com>
2019-05-27 14:14:19 +0800

    第三题 第一次提交

---
+++ src/Container/test3.java
@@ -0,0 +1,20 @@
+package Container;
+
+public class test3 {
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
+class people
+{
+	String name;
+	int count;
+	public people (String name,int count)
+	{
+		super();
+		this.name=name;
+		this.count=count;
+	}
+	}
