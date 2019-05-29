2fe2001f8043c199514333a2468bd7b102e9b217
chm <3036643270@qq.com>
2019-05-29 12:36:20 +0800

    第五题第一次

---
+++ src/Container/test5.java
@@ -0,0 +1,31 @@
+package Container;
+
+public class test5 {
+public static void mian(String[] args)
+{
+	
+}
+}
+class student{
+	int id ;
+	String name;
+	int count;
+	public int getId(){
+		return id;
+	}
+	public void setId(int id) {
+		this.id=id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count ) {
+		this.count=count;
+	}
+}
