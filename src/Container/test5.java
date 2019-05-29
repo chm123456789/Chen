43362bf88eaff687d273c89c86ead5f1fec52c4a
chm <3036643270@qq.com>
2019-05-29 13:37:38 +0800

    第二次

--- src/Container/test5.java
+++ src/Container/test5.java
@@ -1,19 +1,20 @@
 package Container;
 
 public class test5 {
+	public static final Object id=null;
 public static void mian(String[] args)
 {
 	
 }
 }
-class student{
-	int id ;
+class student implements Comparable<Object>{
+	String  id ;
 	String name;
 	int count;
-	public int getId(){
+	public String  getId(){
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id=id;
 	}
 	public String getName() {
@@ -28,4 +29,72 @@ class student{
 	public void setCount(int count ) {
 		this.count=count;
 	}
-}
+	public String toString() {
+		return "student [id=" + id + ", name=" + name + ", count=" + count + "]";
+	}
+	public student(String  id,String name,int count)
+	{
+		super();
+		this.id=id;
+		this.name=name;
+		this.count=count;
+	}
+	@Override
+	public int hashCode() {
+
+		int result;
+
+		result = (this.name == null ? 0 : this.name.hashCode());
+
+		result += this.id.hashCode();
+
+		return result;
+
+	}
+
+	@Override
+
+	public boolean equals(Object o) {
+
+		if (!(o instanceof test5)) {
+			return false;
+		}
+
+		test5 s = (test5) o;
+
+		if (this.id.equals(s.id)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		
+		student s = (student )o;
+
+        int result = 0;
+
+        result = this.count > s.count ? 1 : (this.count == s.count ? 0 : -1);
+
+        return result;
+	}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
