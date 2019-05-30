728bc739b3c558ba37545f763d1c42e1705c813d
chm <3036643270@qq.com>
2019-05-30 09:03:49 +0800

    第六题 第一次

---
+++ src/Container/test6.java
@@ -0,0 +1,24 @@
+package Container;
+class person
+{
+	int age;
+	String name;
+	public person (int age,String name) {
+		super();
+		this.age=age;
+		this.name=name;
+		
+	}
+	public String toString() {
+		return "person [age=" + age + ", name=" + name + "]";
+	}
+	
+}
+
+
+public class test6 {
+	public static void main(String[] args) {
+		
+	
+	}
+}
