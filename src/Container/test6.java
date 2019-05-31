2e0174be16fff33718dca4edd3e6d5b03785595f
chm <3036643270@qq.com>
2019-05-31 21:27:59 +0800

    第二次

--- src/Container/test6.java
+++ src/Container/test6.java
@@ -1,9 +1,9 @@
 package Container;
 class person
 {
-	int age;
+	Integer age;
 	String name;
-	public person (int age,String name) {
+	public person (Integer age,String name) {
 		super();
 		this.age=age;
 		this.name=name;
@@ -12,13 +12,30 @@ class person
 	public String toString() {
 		return "person [age=" + age + ", name=" + name + "]";
 	}
-	
+	public boolean equals(Object o)
+		{ if(this==o)
+		{
+			return true;
+		}
+		if(o==null||getClass()!=o.getClass())
+		{
+			return false;
+		}
+		person p=(person) o;
+		return age.equals(p.age);
+			
+		}
+	public int hashCode() {
+		return age.hashCode();
+	}
 }
 
 
 public class test6 {
 	public static void main(String[] args) {
 		
-	
+		
 	}
+
+	
 }
